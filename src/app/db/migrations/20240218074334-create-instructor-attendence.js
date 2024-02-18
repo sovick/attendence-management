c650f0e6e5f22722b2cd53f2b7cbff31e74c66d1
dev-sk <dev.sovick@gmail.com>
2024-02-18 13:22:15 +0530

    InstructorAttendence modal and migration added

---
+++ src/app/db/migrations/20240218074334-create-instructor-attendence.js
@@ -0,0 +1,38 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("InstructorAttendences", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      date: {
+        type: Sequelize.DATE(6),
+      },
+      operation: {
+        type: Sequelize.STRING,
+      },
+      instructorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Instructors",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("InstructorAttendences");
+  },
+};
---
+++ src/app/db/models/instructorattendence.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class InstructorAttendence extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  InstructorAttendence.init(
+    {
+      date: DataTypes.DATE(6),
+      operation: DataTypes.STRING,
+      instructorId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "InstructorAttendence",
+    }
+  );
+  return InstructorAttendence;
+};
